0f830b0d4c6f08e58d9b3e5cbce99c1f8c49b19a
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-11-09 14:44:52 +0700

    Update InstructorEditInfo Update form

--- app/src/main/java/men/ngopi/aviedb/fitnesin/InstructorEditInfo.java
+++ app/src/main/java/men/ngopi/aviedb/fitnesin/InstructorEditInfo.java
@@ -4,15 +4,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class InstructorEditInfo extends Activity implements View.OnClickListener {
     private TextView tv;
+    private Spinner mGenderSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructor_edit_info);
+
+        mGenderSpinner = findViewById(R.id.genderSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.gender, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mGenderSpinner.setAdapter(adapter);
 
         tv = findViewById(R.id.back);
         tv.setOnClickListener(this);
